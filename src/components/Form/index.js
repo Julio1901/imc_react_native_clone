e56a738c668b204046b7a1b0a1934c825d4b7c11
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-05-31 12:26:08 -0300

    Vibration API implemented

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,41 +1,54 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Keyboard, Vibration} from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
 export default function Form(){
     
-    const [height, setHeight] = useState(null)
-    const [weight, setWeight] = useState(null)
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
     const [messageImc, setBMIStatus] = useState("Please enter weight and height");
-    const [bmi, setBMI] = useState(null)
+    const [bmi, setBMI] = useState(null);
     const [textButton, setButtonText] = useState("Calculate");
+    const [errorMessage, setErrorMessage] = useState(null);
 
     function calculateBMI(){
         return setBMI((weight/(height * height)).toFixed(2))
     }
+
+    function verificationImc(){
+        if(bmi == null){
+            Vibration.vibrate();
+            setErrorMessage("Campo obrigatório*")
+        }
+    }
+
     function validateBMI() {
         if (weight !== null && height !== null) {
-            calculateBMI();
-            setHeight(null);
-            setWeight(null);
-            setBMIStatus("Your BMI is: ");
-            setButtonText("Calculate Again");
+            calculateBMI()
+            setHeight(null)
+            setWeight(null)
+            setBMIStatus("Your BMI is: ")
+            setButtonText("Calculate Again")
+            setErrorMessage(null)
             return;
         }
-        setBMI(null);
-        setButtonText("Calculate");
-        setBMIStatus("Please enter weight and height");
+        verificationImc()
+        setBMI(null)
+        setButtonText("Calculate")
+        setBMIStatus("Please enter weight and height")
     }
 
     return (
       <View style={styles.formContext}>
         <View style={styles.form}>
             <Text style={styles.formLabel}>Height</Text>
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
             <TextInput style={styles.input} onChangeText={setHeight} value={height} placeholder="1.75" keyboardType="numeric"/>
             <Text style={styles.formLabel}>Weight</Text>
+            <Text style={styles.errorMessage}>{errorMessage}</Text>
             <TextInput style={styles.input} onChangeText={setWeight} value={weight} placeholder="75.365" keyboardType="numeric"/>
-            <TouchableOpacity style={styles.buttonCalculator} onPress={() => { validateBMI()}}>
+            <TouchableOpacity style={styles.buttonCalculator} onPress={() => { validateBMI(), Keyboard.dismiss()}}>
                 <Text style={styles.textButtonCalculator}>{textButton}</Text>
             </TouchableOpacity>
         </View>
