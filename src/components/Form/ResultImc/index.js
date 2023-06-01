ae183674d288314b30742d1aec1878d12f61cd48
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-05-31 21:39:34 -0300

    Shared implemented

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Share } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props){
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message:"My BMI is: " + props.resultImc
+        })
+    }
+
     return (
         <View style={styles.resultImc}>
+            <View style={styles.boxShareButton}>
+                {props.resultImc != null ?
+                <TouchableOpacity style={styles.share} onPress={onShare}>
+                    <Text style={styles.shareText}>Share</Text>
+                </TouchableOpacity>
+                :
+                 <View/>
+                }
+            </View>
             <Text style={styles.information}>{props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
